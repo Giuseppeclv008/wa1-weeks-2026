51c295558e516e788ae8535dcbc21313af2755d7
s354774 <s354774@studenti.polito.it>
2026-02-24 20:05:25 +0100

    excercise resolved

--- week01/better-scores.js
+++ week01/better-scores.js
@@ -16,7 +16,58 @@ Develop a small JavaScript program to manage the scores given to your user on we
 */
 
 
-let scores = [3, -2, 10, 0, -5, 7, 4, -1, 9, 6] ;
+const scores = [3, -2, 10, 0, -5, -8, 7, 4, -1, 9, 6] ;
+
+const newscores = [] ; // make a copy of the scores array, but only with the non-negative scores
+for (const value of scores) {
+  if ( value >= 0 ) {
+    newscores.push(value) ;
+  }
+}
+
+
+
+for(const c of [1,2]){
+  const min = Math.min(...newscores) ; // find the minimum value in the newscores array
+  const pos_smallest = newscores.indexOf(min) ;
+  console.log(`Removing the smallest score: ${min} at position ${pos_smallest}`) ;
+  newscores.splice(pos_smallest, 1) ; // remove the smallest score from the newscores array
+}
+let avg  = 0.0; 
+for (const value of newscores) {
+  avg += value ;
+}
+avg /= newscores.length ;
+avg = Math.round(avg) ;
+console.log(avg);
+
+// scores.filter(value => value >= 0) ; also a valid approach 
+// Remove negative scores
+
+// for (let i = 0; i < (scores.length - newscores.length); i++) { // the condition is evaluated at each iteration, the push reduces the value of 
+//   newscores.push(avg) ;
+// }
+
+while(scores.length > newscores.length) { 
+  newscores.push(avg) ;
+}
+
 
 console.log(scores)
+console.log(newscores)
+
+// for ( let i = 0; i<  newscores.length; i++ ) { // this approach is not optimal, but it is simple and straightforward
+//                                                 // can induce bugs if not careful
+//   const value = newscores[i] ;
+
+//   if ( value < 0 ) {
+//     newscores.splice(i, 1) ;
+//     i--; 
+//   }
+
+//   console.log(newscores) ;
+// }
+
+
+
 
--- week01/my-users.js
+++ week01/my-users.js
@@ -18,4 +18,39 @@ Develop a small JS program to manage the list of users in a website.
 
 const names = "Luigi De Russis, Francesca Russo, Fulvio Corno, Luca Scibetta, Alberto Monge Roffarello" ;
 
-console.log(names)
+const list_names = names.split(",") ;
+console.log(list_names)
+
+for (let i = 0; i< list_names.length; i++) {
+  list_names[i] = list_names[i].trim() ;
+
+}
+console.log(list_names)
+const acronyms = [] ;
+
+for( const value of list_names) {
+ 
+  let name_parts = value.split(" ") ;
+  let acronym = "" ;
+
+  for (const part of name_parts) {
+    acronym += part[0] ;
+  }
+
+  acronyms.push(acronym.toUpperCase()) ;
+
+}
+acronyms.sort() ;
+console.log(acronyms) ;
+
+// we can also implement a solution by means of a map 
+// const list_2 = []
+// for (const name of list_names) {
+//   list_2.push(name.trim()) ;
+// }
+
+
+// doesn't work because name is a copy of the value in the array, not a reference to the value in the arraytt
+// console.log(list_name of list_names) {
+//   name.trim() ; // remove extra spaces from the name if we use name = name.trim() we modify the reference 
+// }
